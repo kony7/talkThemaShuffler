e51d0f9004c2acbd5f7a4cf93e51c499111ddeb4
Kony <fzf55747@kwansei.ac.jp>
2021-02-28 19:18:54 +0900

    Initial Commit

---
+++ talkThema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  talkThema
+//
+//  Created by 小西星七 on 2021/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
